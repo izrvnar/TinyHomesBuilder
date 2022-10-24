3b9f09e7caa2cd5c0c908f33feccf73eafe8c0ac
Ivan Zrvnar <w0778779@myscc.ca>
2022-10-24 14:35:52 -0400

    Added custom theme * added custom colours to the primary theme of the app

--- app/src/main/java/com/zrvnar/tinyhousebuilder/fragments/HomeFragment.java
+++ app/src/main/java/com/zrvnar/tinyhousebuilder/fragments/HomeFragment.java
@@ -1,4 +1,4 @@
-package com.zrvnar.tinyhousebuilder;
+package com.zrvnar.tinyhousebuilder.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.zrvnar.tinyhousebuilder.R;
 
 /**
  * A simple {@link Fragment} subclass.
