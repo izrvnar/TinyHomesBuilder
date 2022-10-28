376c26c70898db848eb357f1e9d1ac8a53483150
Ivan Zrvnar <w0778779@myscc.ca>
2022-10-28 18:59:13 -0400

    Added Settings fragment * Created settings fragment * Added it to navigation * Tested and working properly

--- app/src/main/java/com/zrvnar/tinyhousebuilder/MainActivity.java
+++ app/src/main/java/com/zrvnar/tinyhousebuilder/MainActivity.java
@@ -1,12 +1,14 @@
 package com.zrvnar.tinyhousebuilder;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -54,6 +56,12 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment_content_main);
+        return NavigationUI.onNavDestinationSelected(item, navController) || super.onOptionsItemSelected(item);
     }
 
     @Override
